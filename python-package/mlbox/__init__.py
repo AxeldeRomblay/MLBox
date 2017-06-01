510a9b6a999ec8e3905bbd31257d7bdab094abe1
Axel <axelderomblay@gmail.com>
2017-06-01 22:49:42 +0200

    Add files via upload

---
+++ python-package/mlbox/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Axel ARONIO DE ROMBLAY"""
+__email__ = 'axelderomblay@gmail.com'
+__version__ = '0.2.2'
+
+
+from preprocessing import *
+from encoding import *
+from optimisation import *
+from prediction import *
+from model import *
