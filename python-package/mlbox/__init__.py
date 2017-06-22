7df29d695c4af5b9c55cf25d3936580dcbc202ca
Axel <axelderomblay@gmail.com>
2017-06-22 17:35:31 +0200

    Update __init__.py

--- python-package/mlbox/__init__.py
+++ python-package/mlbox/__init__.py
@@ -4,7 +4,6 @@ __author__ = """Axel ARONIO DE ROMBLAY"""
 __email__ = 'axelderomblay@gmail.com'
 __version__ = '0.2.2'
 
-
 from preprocessing import *
 from encoding import *
 from optimisation import *
