a5426340a6058b1fc118e62e80199420ba44d182
Axel <axelderomblay@gmail.com>
2017-07-05 17:12:06 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,17 +16,6 @@
 import sys
 import os
 
-from mock import Mock as MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-MOCK_MODULES = ['lightgbm', 'xgboost','numpy', 'pandas']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
-
 
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
