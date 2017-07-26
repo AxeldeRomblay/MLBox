7f9e5609c534cc7349fdec82cdc162ba2e865672
Nicolas Cherel <nicolas.cherel@telecom-paristech.fr>
2017-07-26 04:25:47 +0200

    fix typo

--- python-package/mlbox/model/supervised/regression/__init__.py
+++ python-package/mlbox/model/supervised/regression/__init__.py
@@ -1,5 +1,5 @@
-from .feature_selector import Reg_Feature_Selector
+from .feature_selector import Reg_feature_selector
 from .regressor import Regressor
 from .stacking_regressor import StackingRegressor
 
-__all__ = ['Reg_Feature_Selector', 'Regressor', 'StackingRegressor']
+__all__ = ['Reg_feature_selector', 'Regressor', 'StackingRegressor']
