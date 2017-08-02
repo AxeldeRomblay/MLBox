b1bbb8eac0e9b33307a49de9988aa757630a5a6a
Axel <axelderomblay@gmail.com>
2017-08-02 10:37:06 +0200

    get rid of log_progress bar

--- python-package/mlbox/preprocessing/drift/rde_cv.py
+++ python-package/mlbox/preprocessing/drift/rde_cv.py
@@ -6,64 +6,6 @@
 import numpy as np
 from sklearn.model_selection import KFold, cross_val_score
 from .drift_threshold import DriftThreshold
-from ipywidgets import IntProgress, HTML, VBox
-from IPython.display import display
-
-
-def log_progress(sequence, every=None, size=None):
-
-    """
-    Returns a log progress bar for computations
-
-    """
-
-    is_iterator = False
-
-    if size is None:
-        try:
-            size = len(sequence)
-        except TypeError:
-            is_iterator = True
-    if size is not None:
-        if every is None:
-            if size <= 200:
-                every = 1
-            else:
-                every = size / 200  # every 0.5%
-    else:
-        assert every is not None, 'sequence is iterator, set every'
-
-    if is_iterator:
-        progress = IntProgress(min=0, max=1, value=1)
-        progress.bar_style = 'info'
-    else:
-        progress = IntProgress(min=0, max=size, value=0)
-
-    label = HTML()
-    box = VBox(children=[label, progress])
-    display(box)
-
-    index = 0
-    try:
-        for index, record in enumerate(sequence, 1):
-            if index == 1 or index % every == 0:
-                if is_iterator:
-                    label.value = '{index} / ?'.format(index=index)
-                else:
-                    progress.value = index
-                    label.value = u'{index} / {size}'.format(
-                        index=index,
-                        size=size
-                    )
-            yield record
-    except:
-        progress.bar_style = 'danger'
-        raise
-    else:
-        progress.bar_style = 'success'
-        progress.value = index
-        label.value = str(index or '?')
-
 
 class RDECV():
 
@@ -246,7 +188,7 @@ class RDECV():
 
         countRemoveVar = 0
 
-        for col in log_progress(col_drifts[:idEndLoop]):
+        for col in col_drifts[:idEndLoop]:
 
             self.__dropList.append(col)
             self.__keepList.append(col)
