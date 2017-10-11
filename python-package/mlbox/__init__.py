bd743966096fd81433766213d208fd5d9a34e76f
Axel <axelderomblay@gmail.com>
2017-10-11 19:11:32 +0200

    version 5.3

--- python-package/mlbox/__init__.py
+++ python-package/mlbox/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Axel ARONIO DE ROMBLAY"""
 __email__ = 'axelderomblay@gmail.com'
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 
 from .preprocessing import *
 from .encoding import *
