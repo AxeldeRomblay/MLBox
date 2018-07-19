d40c18d3bc7d2bf3e19a49fc8cb2f1efe9d7086b
Axel <axelderomblay@gmail.com>
2018-07-19 10:30:51 +0200

    Update setup.py

--- python-package/setup.py
+++ python-package/setup.py
@@ -25,7 +25,7 @@ class OverrideInstallCommand(install):
         failed = []
 
         for req in requirements:
-            if pip.main(["install", req]) == 1:
+            if pip._internal.main(["install", req]) == 1:
                 failed.append(req)
 
         if len(failed) > 0:
