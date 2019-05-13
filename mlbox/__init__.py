4e001f6dd72ebf2b9c1bd17258a5bf4607d83d4a
Henri-Gerard <hgerard.pro@gmail.com>
2019-05-13 11:26:03 +0200

    [FIX] Fix bug installation travis

--- mlbox/__init__.py
+++ mlbox/__init__.py
@@ -4,8 +4,8 @@ __author__ = """Axel ARONIO DE ROMBLAY"""
 __email__ = 'axelderomblay@gmail.com'
 __version__ = '0.6.2'
 
-from .preprocessing import *
-from .encoding import *
-from .optimisation import *
-from .prediction import *
-from .model import *
+# from .preprocessing import *
+# from .encoding import *
+# from .optimisation import *
+# from .prediction import *
+# from .model import *
