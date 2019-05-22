fac2fd41477d5aaebd39cc0ec32f96321d3a8c99
Henri-Gerard <hgerard.pro@gmail.com>
2019-05-22 17:32:36 +0200

    [DEV] Add test_optimiser and test_predictor

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -4,56 +4,38 @@
 # License: BSD 3 clause
 import pytest
 import pandas as pd
-import numpy as np
 
+from mlbox.prediction.predictor import Predictor
 from mlbox.optimisation.optimiser import Optimiser
 from mlbox.preprocessing.drift_thresholder import Drift_thresholder
 from mlbox.preprocessing.reader import Reader
 
 
-def test_init_optimiser():
-    with pytest.warns(UserWarning) as record:
-        optimiser = Optimiser()
-    assert len(record) == 1
-    assert not optimiser.scoring
-    assert optimiser.n_folds == 2
-    assert optimiser.random_state == 1
-    assert optimiser.to_path == "save"
-    assert optimiser.verbose
+def test_init_predictor():
+    predictor = Predictor()
+    assert predictor.to_path == "save"
+    assert predictor.verbose
 
 
-def test_get_params_optimiser():
-    with pytest.warns(UserWarning) as record:
-        optimiser = Optimiser()
-    assert len(record) == 1
-    dict = {'scoring': None,
-            'n_folds': 2,
-            'random_state': 1,
-            'to_path': "save",
+def test_get_params_predictor():
+    predictor = Predictor()
+    dict = {'to_path': "save",
             'verbose': True}
-    assert optimiser.get_params() == dict
+    assert predictor.get_params() == dict
 
 
-def test_set_params_optimiser():
+def test_set_params_predictor():
+    predictor = Predictor()
+    predictor.set_params(to_path="name")
+    assert predictor.to_path == "name"
+    predictor.set_params(verbose=False)
+    assert not predictor.verbose
     with pytest.warns(UserWarning) as record:
-        optimiser = Optimiser()
-    assert len(record) == 1
-    optimiser.set_params(scoring='accuracy')
-    assert optimiser.scoring == 'accuracy'
-    optimiser.set_params(n_folds=3)
-    assert optimiser.n_folds == 3
-    optimiser.set_params(random_state=2)
-    assert optimiser.random_state == 2
-    optimiser.set_params(to_path="name")
-    assert optimiser.to_path == "name"
-    optimiser.set_params(verbose=False)
-    assert not optimiser.verbose
-    with pytest.warns(UserWarning) as record:
-        optimiser.set_params(wrong_key=3)
+        predictor.set_params(wrong_key=3)
     assert len(record) == 1
 
 
-def test_evaluate_and_optimise():
+def test_fit_predict():
     reader = Reader(sep=",")
     dict = reader.train_test_split(Lpath=["data_for_tests/train.csv",
                                           "data_for_tests/test.csv"],
@@ -65,7 +47,6 @@ def test_evaluate_and_optimise():
         opt = Optimiser(scoring='accuracy', n_folds=3)
     assert len(record) == 1
     score = opt.evaluate(None, dict)
-    assert np.round(score, 6) == 0.784512
 
     space = {'ne__numerical_strategy': {"search": "choice", "space": [0]},
              'ce__strategy': {"search": "choice",
@@ -79,5 +60,7 @@ def test_evaluate_and_optimise():
 
              }
 
-    best = opt.optimise(space, dict, 1)
-    assert type(best) == type(dict)
+    optimal_hyper_parameters = opt.optimise(space, dict, 1)
+
+    predictor = Predictor()
+    predictor.fit_predict(optimal_hyper_parameters, dict)
