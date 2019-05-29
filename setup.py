e2e47c5875c058abb2df991aef9fc48a99b23f05
Axel DEROMBLAY <a.deromblay@wks-121.corp.dailymotion.com>
2019-05-29 17:14:11 +0200

    fix python versions

--- setup.py
+++ setup.py
@@ -64,8 +64,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.6'
     ],
     test_suite='tests',
     tests_require=requirements
