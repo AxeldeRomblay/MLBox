66b20ae3b5dc69b13c88c25fc75cb36376a03e36
Henri-Gerard <hgerard.pro@gmail.com>
2019-06-07 15:16:11 +0200

    [UPD] Comment examples

--- examples/classification/classification.py
+++ examples/classification/classification.py
@@ -1,22 +1,50 @@
+"""A classification example using mlbox."""
 from mlbox.preprocessing import Reader
 from mlbox.preprocessing import Drift_thresholder
 from mlbox.optimisation import Optimiser
 from mlbox.prediction import Predictor
 
-paths = ["train.csv", "test.csv"]
+# Paths to the train set and the test set.
+paths = ["train_classification.csv", "test_classification.csv"]
+# Name of the feature to predict.
+# This columns should only be present in the train set.
 target_name = "Survived"
 
 # Reading and cleaning all files
+# Declare a reader for csv files
 rd = Reader(sep=',')
-df = rd.train_test_split(paths, target_name)
+# Return a dictionnary containing three entries
+# dict["train"] contains training samples withtout target columns
+# dict["test"] contains testing elements withtout target columns
+# dict["target"] contains target columns for training samples.
+dict = rd.train_test_split(paths, target_name)
 
 dft = Drift_thresholder()
-df = dft.fit_transform(df)
+dict = dft.fit_transform(dict)
 
 # Tuning
+# Declare an optimiser. Scoring possibilities for classification lie in :
+# {"accuracy", "roc_auc", "f1", "log_loss", "precision", "recall"}
 opt = Optimiser(scoring='accuracy', n_folds=3)
-opt.evaluate(None, df)
+opt.evaluate(None, dict)
 
+# Space of hyperparameters
+# The keys must respect the following syntax : "enc__param".
+#   "enc" = "ne" for na encoder
+#   "enc" = "ce" for categorical encoder
+#   "enc" = "fs" for feature selector [OPTIONAL]
+#   "enc" = "stck"+str(i) to add layer n°i of meta-features [OPTIONAL]
+#   "enc" = "est" for the final estimator
+#   "param" : a correct associated parameter for each step.
+#   Ex: "max_depth" for "enc"="est", ...
+# The values must respect the syntax: {"search":strategy,"space":list}
+#   "strategy" = "choice" or "uniform". Default = "choice"
+#   list : a list of values to be tested if strategy="choice".
+#   Else, list = [value_min, value_max].
+# Available strategies for ne_numerical_strategy are either an integer, a float
+#   or in {'mean', 'median', "most_frequent"}
+# Available strategies for ce_strategy are:
+#   {"label_encoding", "dummification", "random_projection", entity_embedding"}
 space = {'ne__numerical_strategy': {"search": "choice", "space": [0]},
          'ce__strategy': {"search": "choice",
                           "space": ["label_encoding",
@@ -29,7 +57,13 @@ space = {'ne__numerical_strategy': {"search": "choice", "space": [0]},
 
          }
 
-best = opt.optimise(space, df, 15)
+# Optimises hyper-parameters of the whole Pipeline with a given scoring
+# function. Algorithm used to optimize : Tree Parzen Estimator.
+#
+# IMPORTANT : Try to avoid dependent parameters and to set one feature
+# selection strategy and one estimator strategy at a time.
+best = opt.optimise(space, dict, 15)
 
+# Make prediction and save the results in save folder.
 prd = Predictor()
-prd.fit_predict(best, df)
+prd.fit_predict(best, dict)
--- examples/regression/regression.py
+++ examples/regression/regression.py
@@ -1,3 +1,4 @@
+"""A regression example using mlbox."""
 import numpy as np
 
 from mlbox.preprocessing import Reader
@@ -6,15 +7,23 @@ from mlbox.optimisation import make_scorer
 from mlbox.optimisation import Optimiser
 from mlbox.prediction import Predictor
 
-paths = ["train.csv", "test.csv"]
+# Paths to the train set and the test set.
+paths = ["train_regression.csv", "test_regression.csv"]
+# Name of the feature to predict.
+# This columns should only be present in the train set.
 target_name = "SalePrice"
 
-# Reading and cleaning files
+# Reading and cleaning all files
+# Declare a reader for csv files
 rd = Reader(sep=',')
-df = rd.train_test_split(paths, target_name)
+# Return a dictionnary containing three entries
+# dict["train"] contains training samples withtout target columns
+# dict["test"] contains testing elements withtout target columns
+# dict["target"] contains target columns for training samples.
+dict = rd.train_test_split(paths, target_name)
 
 dft = Drift_thresholder()
-df = dft.fit_transform(df)
+dict = dft.fit_transform(dict)
 
 # Tuning
 mape = make_scorer(lambda y_true,
@@ -23,10 +32,29 @@ mape = make_scorer(lambda y_true,
                                       )/len(y_true),
                    greater_is_better=False,
                    needs_proba=False)
+# Declare an optimiser. You can declare your own score
+# as presented here or use one in
+# {"mean_absolute_error", "mean_squared_error","median_absolute_error","r2"}
 opt = Optimiser(scoring=mape, n_folds=3)
+opt.evaluate(None, dict)
 
-opt.evaluate(None, df)
-
+# Space of hyperparameters
+# The keys must respect the following syntax : "enc__param".
+#   "enc" = "ne" for na encoder
+#   "enc" = "ce" for categorical encoder
+#   "enc" = "fs" for feature selector [OPTIONAL]
+#   "enc" = "stck"+str(i) to add layer n°i of meta-features [OPTIONAL]
+#   "enc" = "est" for the final estimator
+#   "param" : a correct associated parameter for each step.
+#   Ex: "max_depth" for "enc"="est", ...
+# The values must respect the syntax: {"search":strategy,"space":list}
+#   "strategy" = "choice" or "uniform". Default = "choice"
+#   list : a list of values to be tested if strategy="choice".
+#   Else, list = [value_min, value_max].
+# Available strategies for ne_numerical_strategy are either an integer, a float
+#   or in {'mean', 'median', "most_frequent"}
+# Available strategies for ce_strategy are:
+#   {"label_encoding", "dummification", "random_projection", entity_embedding"}
 space = {
         'ne__numerical_strategy': {"search": "choice",
                                    "space": [0]},
@@ -41,7 +69,13 @@ space = {
 
         }
 
-best = opt.optimise(space, df, 15)
+# Optimises hyper-parameters of the whole Pipeline with a given scoring
+# function. Algorithm used to optimize : Tree Parzen Estimator.
+#
+# IMPORTANT : Try to avoid dependent parameters and to set one feature
+# selection strategy and one estimator strategy at a time.
+best = opt.optimise(space, dict, 15)
 
+# Make prediction and save the results in save folder.
 prd = Predictor()
-prd.fit_predict(best, df)
+prd.fit_predict(best, dict)
