1e2c0aa956444b133509b6c48bfa71d8b471f634
Henri-Gerard <hgerard.pro@gmail.com>
2019-06-07 13:03:44 +0200

    [FIX] Fix test_predictor for python2.7

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -4,17 +4,19 @@
 # Author: Axel ARONIO DE ROMBLAY <axelderomblay@gmail.com>
 # Author: Henri GERARD <hgerard.pro@gmail.com>
 # License: BSD 3 clause
+import sys
 import pytest
 import numpy as np
 import pandas as pd
-
-from unittest.mock import patch
 
 from mlbox.prediction.predictor import Predictor
 from mlbox.optimisation.optimiser import Optimiser
 from mlbox.preprocessing.drift_thresholder import Drift_thresholder
 from mlbox.preprocessing.reader import Reader
 from mlbox.optimisation import make_scorer
+
+if sys.version_info[0] >= 3:
+    from unittest.mock import patch
 
 
 set_backend = "import matplotlib\nmatplotlib.use('Agg')\n"
@@ -84,46 +86,91 @@ def test_fit_predict_predictor_classification():
     assert np.shape(pred_df) == (418, 4)
 
 
-@patch('matplotlib.pyplot.show')
-def test_fit_predict_predictor_regression(mock_show):
-    """Test fit_predict method of Predictor class for regression."""
-    rd = Reader(sep=',')
-    dict = rd.train_test_split(Lpath=["data_for_tests/train_regression.csv",
-                                      "data_for_tests/test_regression.csv"],
-                               target_name="SalePrice")
+if sys.version_info[0] >= 3:
+    @patch('matplotlib.pyplot.show')
+    def test_fit_predict_predictor_regression(mock_show):
+        """Test fit_predict method of Predictor class for regression."""
+        rd = Reader(sep=',')
+        dt = rd.train_test_split(Lpath=["data_for_tests/train_regression.csv",
+                                        "data_for_tests/test_regression.csv"],
+                                 target_name="SalePrice")
 
-    drift_thresholder = Drift_thresholder()
-    df = drift_thresholder.fit_transform(dict)
+        drift_thresholder = Drift_thresholder()
+        df = drift_thresholder.fit_transform(dt)
 
-    mape = make_scorer(lambda y_true,
-                       y_pred: 100*np.sum(
-                                          np.abs(y_true-y_pred)/y_true
-                                          )/len(y_true),
-                       greater_is_better=False,
-                       needs_proba=False)
-    opt = Optimiser(scoring=mape, n_folds=3)
+        mape = make_scorer(lambda y_true,
+                           y_pred: 100*np.sum(
+                                              np.abs(y_true-y_pred)/y_true
+                                              )/len(y_true),
+                           greater_is_better=False,
+                           needs_proba=False)
+        opt = Optimiser(scoring=mape, n_folds=3)
 
-    opt.evaluate(None, df)
+        opt.evaluate(None, df)
 
-    space = {
-            'ne__numerical_strategy': {"search": "choice",
-                                       "space": [0]},
-            'ce__strategy': {"search": "choice",
-                             "space": ["label_encoding",
-                                       "random_projection",
-                                       "entity_embedding"]},
-            'fs__threshold': {"search": "uniform",
-                              "space": [0.01, 0.3]},
-            'est__max_depth': {"search": "choice",
-                               "space": [3, 4, 5, 6, 7]}
+        space = {
+                'ne__numerical_strategy': {"search": "choice",
+                                           "space": [0]},
+                'ce__strategy': {"search": "choice",
+                                 "space": ["label_encoding",
+                                           "random_projection",
+                                           "entity_embedding"]},
+                'fs__threshold': {"search": "uniform",
+                                  "space": [0.01, 0.3]},
+                'est__max_depth': {"search": "choice",
+                                   "space": [3, 4, 5, 6, 7]}
 
-            }
+                }
 
-    best = opt.optimise(space, df, 1)
+        best = opt.optimise(space, df, 1)
 
-    prd = Predictor(verbose=True)
-    prd.fit_predict(best, df)
-    pred_df = pd.read_csv("save/SalePrice_predictions.csv")
-    assert np.all(list(pred_df.columns) == ['Unnamed: 0',
-                                            'SalePrice_predicted'])
-    assert np.shape(pred_df) == (1459, 2)
+        prd = Predictor(verbose=True)
+        prd.fit_predict(best, df)
+        pred_df = pd.read_csv("save/SalePrice_predictions.csv")
+        assert np.all(list(pred_df.columns) == ['Unnamed: 0',
+                                                'SalePrice_predicted'])
+        assert np.shape(pred_df) == (1459, 2)
+
+else:
+    def test_fit_predict_predictor_regression():
+        """Test fit_predict method of Predictor class for regression."""
+        rd = Reader(sep=',')
+        dt = rd.train_test_split(Lpath=["data_for_tests/train_regression.csv",
+                                        "data_for_tests/test_regression.csv"],
+                                 target_name="SalePrice")
+
+        drift_thresholder = Drift_thresholder()
+        df = drift_thresholder.fit_transform(dt)
+
+        mape = make_scorer(lambda y_true,
+                           y_pred: 100*np.sum(
+                                              np.abs(y_true-y_pred)/y_true
+                                              )/len(y_true),
+                           greater_is_better=False,
+                           needs_proba=False)
+        opt = Optimiser(scoring=mape, n_folds=3)
+
+        opt.evaluate(None, df)
+
+        space = {
+                'ne__numerical_strategy': {"search": "choice",
+                                           "space": [0]},
+                'ce__strategy': {"search": "choice",
+                                 "space": ["label_encoding",
+                                           "random_projection",
+                                           "entity_embedding"]},
+                'fs__threshold': {"search": "uniform",
+                                  "space": [0.01, 0.3]},
+                'est__max_depth': {"search": "choice",
+                                   "space": [3, 4, 5, 6, 7]}
+
+                }
+
+        best = opt.optimise(space, df, 1)
+
+        prd = Predictor(verbose=False)
+        prd.fit_predict(best, df)
+        pred_df = pd.read_csv("save/SalePrice_predictions.csv")
+        assert np.all(list(pred_df.columns) == ['Unnamed: 0',
+                                                'SalePrice_predicted'])
+        assert np.shape(pred_df) == (1459, 2)
