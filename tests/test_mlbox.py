048869b8fbde5350c6c5945de28475bf5ac05932
Axel DEROMBLAY <a.deromblay@wks-121.corp.dailymotion.com>
2019-06-25 16:40:57 +0200

    [FIX] fix import mlbox test

--- tests/test_mlbox.py
+++ tests/test_mlbox.py
@@ -2,21 +2,5 @@
 # -*- coding: utf-8 -*-
 # License: BSD 3 clause
 """Test import of module mlbox."""
-import pytest
+
 import mlbox
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
