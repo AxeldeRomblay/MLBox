b92cf870e328ac1af8326b8f61e3a50537a4cb4d
Axel DEROMBLAY <a.deromblay@wks-121.corp.dailymotion.com>
2019-06-26 18:05:51 +0200

    [UPD] rm files for PR review

--- mlbox/preprocessing/drift/__init__.py
+++ mlbox/preprocessing/drift/__init__.py
@@ -1,3 +1,2 @@
 from .drift_estimator import *
 from .drift_threshold import *
-from .rde_cv import *
--- tests/test_mlbox.py
+++
@@ -1,6 +0,0 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-# License: BSD 3 clause
-"""Test import of module mlbox."""
-
-import mlbox
