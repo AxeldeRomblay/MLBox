d86af258c11546194b398f73b868db4a85f9b896
AxeldeRomblay <axel.deromblay@dailymotion.com>
2020-04-12 17:36:19 +0200

    add python 3.8

--- setup.py
+++ setup.py
@@ -65,7 +65,8 @@ setup(
 
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     test_suite='tests',
     tests_require=requirements
