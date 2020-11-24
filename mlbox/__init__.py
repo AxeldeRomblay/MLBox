6c864bc869e4c27998610e615bb5e5512554dbcf
AxeldeRomblay <axel.deromblay@dailymotion.com>
2020-11-24 18:41:45 +0100

    [UPD] add mlbox version info

--- mlbox/__init__.py
+++ mlbox/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+with open('../VERSION.txt') as version_file:
+    __version__ = version_file.read()
+
 __author__ = """Axel ARONIO DE ROMBLAY"""
 __email__ = 'axelderomblay@gmail.com'
 
